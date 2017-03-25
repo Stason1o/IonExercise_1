21fe2ee79efe5af6af84d26c9e57e1c10502b203
Stason1o <stasikus1996@gmail.com>
2017-03-25 20:30:31 +0200

    Added two enums,  BookType, PriceStrategyType.

---
+++ src/com/endava/entity/enums/BookType.java
@@ -0,0 +1,10 @@
+package com.endava.entity.enums;
+
+/**
+ * Created by Stas on 25.03.2017.
+ */
+public enum BookType {
+    CLASSIC,
+    FICTION,
+    SCIENCE
+}
---
+++ src/com/endava/entity/enums/PriceStrategyType.java
@@ -0,0 +1,9 @@
+package com.endava.entity.enums;
+
+/**
+ * Created by Stas on 25.03.2017.
+ */
+public enum PriceStrategyType {
+    NORMAL,
+    SPECIAL
+}
